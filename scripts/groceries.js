03639d5bedddb67b629f6298c3d6fcc4166bbd2a
Justin Rackus <95718736+justinrackus@users.noreply.github.com>
2026-01-27 12:53:21 -0500

    added groceries

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -3,29 +3,78 @@
 // A set of ingredients should be added to products		 
 
 var products = [
-	{
-		name: "brocoli",
-		vegetarian: true,
-		glutenFree: true,
-		organic: true, 
-		price: 1.99
-	},
-	{
-		name: "bread",
-		vegetarian: true,
-		glutenFree: false,
-		organic: false,
-		price: 2.35
-	},
-	{
-		name: "salmon",
-		vegetarian: false,
-		glutenFree: true,
-		organic: true,
-		price: 10.00
-	}
+  {
+    name: "brocoli",
+    vegetarian: true,
+    glutenFree: true,
+    organic: true,
+    price: 1.99
+  },
+  {
+    name: "bread",
+    vegetarian: true,
+    glutenFree: false,
+    organic: false,
+    price: 2.35
+  },
+  {
+    name: "salmon",
+    vegetarian: false,
+    glutenFree: true,
+    organic: false,
+    price: 10.00
+  },
+  {
+    name: "tofu",
+    vegetarian: true,
+    glutenFree: true,
+    organic: true,
+    price: 3.49
+  },
+  {
+    name: "chicken breast",
+    vegetarian: false,
+    glutenFree: true,
+    organic: true,
+    price: 7.99
+  },
+  {
+    name: "pasta",
+    vegetarian: true,
+    glutenFree: false,
+    organic: false,
+    price: 2.89
+  },
+  {
+    name: "eggs",
+    vegetarian: true,
+    glutenFree: true,
+    organic: true,
+    price: 4.29
+  },
+  {
+    name: "milk",
+    vegetarian: true,
+    glutenFree: true,
+    organic: false,
+    price: 2.79
+  },
+  {
+    name: "apples",
+    vegetarian: true,
+    glutenFree: true,
+    organic: true,
+    price: 3.99
+  },
+  {
+    name: "ground beef",
+    vegetarian: false,
+    glutenFree: true,
+    organic: false,
+    price: 8.49
+  }
 ];
-	
+
 
 
 // given restrictions provided, make a reduced list of products
